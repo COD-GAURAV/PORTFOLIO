af93727765995caa1a64ae63a7dea1d5bb49dd8a
GAURAV KUMAR <yash35981@gmail.com>
2025-05-31 22:30:21 +0530

    adding typeWritter function in js file

--- logic.js
+++ logic.js
@@ -6,6 +6,7 @@ const home = document.getElementById("home")
 const about = document.getElementById("ABOUT")
 const skill = document.getElementById("SKILLS")
 const contact = document.getElementById("CONTACT")
+let gaurav = document.getElementById("gaurav")
 
 let contentSection = document.getElementById("content")
 
@@ -50,6 +51,42 @@ contact.addEventListener("click",function(){
         behavior:"smooth"
     })
 })
+
+// adding typewritter effect
+
+const type = "GAUARV !"
+
+let adding = true
+
+let index = 0 
+
+function typeWritter(){
+    if(adding){
+        index++
+        if(index > type.length){
+            adding = false
+            index = type.length - 1
+            setTimeout(typeWritter ,500)
+            return
+        }
+    }
+    else{
+        index--
+        if(index < 0){
+            adding = true
+            index = 1
+            setTimeout(typeWritter ,500)
+            return
+        }
+    }
+    console.clear()
+    gaurav.innerHTML = "HI MY NAME IS " + type.substring(0, index)
+
+    setTimeout(typeWritter ,100)
+}
+
+typeWritter()
+
 
 
 
